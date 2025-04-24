3df3b84352ba1134cf539cbd934f5bf1f74aae91
Amoura29 <amira.bouafif7434@istic.ucar.tn>
2025-04-24 11:34:38 +0100

    ffff

---
+++ js/index.js
@@ -0,0 +1,1 @@
+import { ListCategory } from "./listCategory.js";
--- js/listCategory.js
+++ js/listCategory.js
@@ -1,17 +1,16 @@
 export class ListCategory extends HTMLElement{
     constructor(){
         super();
-    }}
-    connectedCallBack(
-        this.innerHTML=`<select>
-        </select>`,
-        this.addEventListener('DOMContentLoaded',async()=>{
-            let url="https://opentdb.com/api_category.php"
-            let r=await((await (fetch(url))).json())
-            for (let i of r.trivia_categories){
-                let code=`<option value=${i.id}>${i.name}</option>`
-                this.querySelector("select").innerHTML+=code
-            }
-        })
-    )
+    }
+    connectedCallback(){
+        this.innerHTML=`<select name="category">
+        </select>`
+        let url="https://opentdb.com/api_category.php"
+        let r=await((await (fetch(url))).json())
+        for (let i of r.trivia_categories){
+            let code=`<option value=${i.id}>${i.name}</option>`
+            this.querySelector("select").innerHTML+=code
+        }
+    }
+}
 customElements.define('list-category', ListCategory);
