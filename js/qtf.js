ce568914999b755741677f615289e37b91b2c669
HajerJemaa <haje.jemaa4380@istic.ucar.tn>
2025-04-24 11:49:32 +0100

    finaaal

--- js/qtf.js
+++ js/qtf.js
@@ -1,5 +1,27 @@
 export class qtf extends HTMLElement{
     constructor(){
-        super()
+        super();
+    }
+    connectedCallback(){
+        this.innerHTML=`
+        Q: <p>${this.getAttribute().question}</p>
+        <button id="b1">True</button>
+        <button id="b2">False</button>`
+        let b1=this.querySelector("#b1");
+        let b2=this.querySelector("#b2");
+        b1.addEventListner("click",()=>{
+            if (this.getAttribute("correct")=="False")
+            alert ("false");
+            else
+            alert("True good job!");     
+        })
+        b2.addEventListner("click",()=>{
+            if (this.getAttribute("correct")=="False")
+                alert("True good job!"); 
+
+            else
+                alert ("false");
+                
+        })
     }
 }
